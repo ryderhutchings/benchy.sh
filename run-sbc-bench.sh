6bda2d51e7e20e24d49966466be49491adbdc90a
Ryder Hutchings <150638428+ryderhutchings@users.noreply.github.com>
2026-04-19 17:39:31 -0600

    Update run-sbc-bench.sh

--- run-sbc-bench.sh
+++ run-sbc-bench.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -e
 
-RESULTS_FILE=~/benchmarks/sbc-bench-results.txt
+RESULTS_FILE=~/Benchy/sbc-bench-results.txt
 
-mkdir -p ~/benchmarks
+mkdir -p ~/Benchy
 
 echo "Running sbc-bench..."
 
-sudo /bin/bash ~/benchmarks/sbc-bench.sh -r 2>&1 | tee "$RESULTS_FILE"
+sudo /bin/bash ~/Benchy/sbc-bench.sh -r 2>&1 | tee "$RESULTS_FILE"
 
 echo ""
 echo "Results saved to $RESULTS_FILE"
